610a8905e951b4a49ffc7b0d260fb0914101b0ff
trancuongk5 <trancuong13112005@gmail.com>
2024-03-09 16:00:48 +0700

    resize header mobile

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// var clickMenu = document.querySelector('#click-menu');
+// var headerShow = document.querySelector('.show-header-mobile');
+
+// function showSelector() {
+//     headerShow.classList.add('showMenuClick');
+// }
+
+// clickMenu.addEventListener('click', showSelector);
+
+// console.log(clickMenu);
+// console.log(headerShow);
