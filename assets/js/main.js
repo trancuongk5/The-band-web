9eef4dfc3743837b49f3c5b8e3efde7ee0b13ca0
trancuongk5 <trancuong13112005@gmail.com>
2024-03-09 16:03:33 +0700

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
-// var clickMenu = document.querySelector('#click-menu');
-// var headerShow = document.querySelector('.show-header-mobile');
-
-// function showSelector() {
-//     headerShow.classList.add('showMenuClick');
-// }
-
-// clickMenu.addEventListener('click', showSelector);
-
-// console.log(clickMenu);
-// console.log(headerShow);
+var clickMenu = document.getElementById('click-menu');
+clickMenu.addEventListener('click', function() {
+    var headerShow = document.querySelector('.show-header-mobile');
+    if (headerShow) {
+        headerShow.classList.toggle('showMenuClick');
+    }
+});
