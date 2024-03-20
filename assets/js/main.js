59aebd0bc15b09be9f4bc33e9669f8af9f80f0dd
trancuongk5 <trancuong13112005@gmail.com>
2024-03-20 15:32:43 +0700

    fix style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 var clickMenu = document.getElementById('click-menu');
 var headerShow = document.querySelector('.show-header-mobile');
-clickMenu.addEventListener('click', function() {
+clickMenu.addEventListener('click', function () {
     if (headerShow) {
         headerShow.classList.toggle('showMenuClick');
     }
